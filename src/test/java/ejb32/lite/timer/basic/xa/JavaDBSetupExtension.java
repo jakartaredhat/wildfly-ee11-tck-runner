625dcf1d1d198216b04784a6f3cfdbcd64358a74
Scott M Stark <starksm64@gmail.com>
2024-08-19 14:06:51 -0600

    Get the JavaDBSetupExtension to run
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

--- src/test/java/ejb32/lite/timer/basic/xa/JavaDBSetupExtension.java
+++ src/test/java/ejb32/lite/timer/basic/xa/JavaDBSetupExtension.java
@@ -1,37 +1,42 @@
 package ejb32.lite.timer.basic.xa;
 
 import com.sun.ts.tests.ejb32.lite.timer.basic.xa.XATimerSuite;
+import com.sun.ts.tests.ejb32.lite.timer.basic.xa.ClientEjbliteservletTest;
 import ee.tck.javadb.JavaDBController;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Logger;
 
 /**
  * Extends the XATimerSuite to start and stop the JavaDB server before and after the suite.
  */
 @Suite
-public class JavaDBSetupExtension extends XATimerSuite implements BeforeAllCallback, ExtensionContext.Store.CloseableResource {
+@SelectClasses({ClientEjbliteservletTest.class})
+public class JavaDBSetupExtension /*extends XATimerSuite*/ implements BeforeAllCallback, ExtensionContext.Store.CloseableResource {
+    static Logger log = Logger.getLogger(JavaDBSetupExtension.class.getName());
     @Override
     public void beforeAll(ExtensionContext extensionContext) throws Exception {
-        System.out.printf("beforeAll suite");
+        log.info("beforeAll suite");
         Path tsHome = Paths.get(System.getProperty("ts.home"));
         Path derbyHome = tsHome.resolve("../glassfish7/javadb");
         JavaDBController controller = new JavaDBController();
-        System.out.println("JavaDBSetupExtension.starting JavaDB with derbyHome="+derbyHome);
+        log.info("JavaDBSetupExtension.starting JavaDB with derbyHome="+derbyHome);
         extensionContext.publishReportEntry("JavaDBSetupExtension.beforeAll", "JavaDBSetupExtension.starting JavaDB with derbyHome="+derbyHome);
         controller.startJavaDB(derbyHome);
-        System.out.println("JavaDBSetupExtension.started JavaDB");
+        log.info("JavaDBSetupExtension.started JavaDB");
         extensionContext.publishReportEntry("JavaDBSetupExtension.beforeAll", "Starting JavaDB");
     }
 
     @Override
     public void close() throws Throwable {
-        System.out.printf("Closing suite");
+        log.info("Closing suite");
         JavaDBController controller = new JavaDBController();
         controller.stopJavaDB();
-        System.out.println("StartStopTest.stopped JavaDB");
+        log.info("StartStopTest.stopped JavaDB");
     }
 }
